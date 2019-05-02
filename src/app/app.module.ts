a8d81311b0e207231ff198cb3cc9d4efc2068866
Yuri Matin <spacemarineharon@mail.ru>
2019-05-02 13:52:06 +0300

    services part done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './modules/tour-of-heroes/components/heroes/heroes.component';
+import { HeroDetailComponent } from './modules/tour-of-heroes/components/hero-detail/hero-detail.component';
+import { MessagesComponent } from './modules/tour-of-heroes/components/messages/messages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/tour-of-heroes/components/heroes/heroes.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core';
+
+import {Hero} from '../../entities/Hero';
+import {HeroService} from '../../services/hero.service';
+import {MessageService} from '../../services/message.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+  selectedHero: Hero;
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService,
+              private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.messageService.add('HeroService: fetched Heroes');
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+  onSelectHero(hero: Hero) {
+    this.selectedHero = hero;
+  }
+}
