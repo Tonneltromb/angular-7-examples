fe1bc425808d923f88dc9770582dddfb8ea971bd
Yuri Matin <spacemarineharon@mail.ru>
2019-07-19 19:22:55 +0300

    добавил пример создания реактивной формы через билдер, добавил пример с валидаторами

--- src/app/modules/examples/components/forms/reactive/reactive-forms.component.ts
+++ src/app/modules/examples/components/forms/reactive/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 
 interface Car {
   mark: string;
@@ -21,10 +21,27 @@ export class ReactiveFormsComponent implements OnInit {
     COLOR: 'color'
   };
   carColors = ['red', 'black', 'green', 'white'];
-  carCreateFormGroup: FormGroup;
+  carCreateForm1: FormGroup;
+  carCreateForm2: FormGroup;
+  carCreateForm3: FormGroup;
 
+  constructor(formBuilder: FormBuilder) {
+    this.carCreateForm2 = formBuilder.group({
+      [this.carFormControl.MARK]: new FormControl(null),
+      [this.carFormControl.MODEL]: new FormControl(null),
+      [this.carFormControl.SPEED]: new FormControl(null),
+      [this.carFormControl.COLOR]: new FormControl(this.carColors[0]),
+    });
+    // validation example
+    this.carCreateForm3 = formBuilder.group({
+      [this.carFormControl.MARK]: new FormControl(null, Validators.required),
+      [this.carFormControl.MODEL]: new FormControl(null, [Validators.required, Validators.minLength(3)]),
+      [this.carFormControl.SPEED]: new FormControl(null),
+      [this.carFormControl.COLOR]: new FormControl(this.carColors[0]),
+    });
+  }
   ngOnInit() {
-    this.carCreateFormGroup = new FormGroup({
+    this.carCreateForm1 = new FormGroup({
       [this.carFormControl.MARK]: new FormControl(null),
       [this.carFormControl.MODEL]: new FormControl(null),
       [this.carFormControl.SPEED]: new FormControl(null),
@@ -32,7 +49,13 @@ export class ReactiveFormsComponent implements OnInit {
     });
   }
 
-  onCreateCarFormSubmit() {
-    console.log(this.carCreateFormGroup.value);
+  onCreateCarForm1Submit() {
+    console.log(this.carCreateForm1.value);
+  }
+  onCreateCarForm2Submit() {
+    console.log(this.carCreateForm2.value);
+  }
+  onCreateCarForm3Submit() {
+    console.log(this.carCreateForm3.value);
   }
 }
